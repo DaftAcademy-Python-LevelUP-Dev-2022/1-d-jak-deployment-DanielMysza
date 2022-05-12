815561b4d65119d489350f3e6426bb3c8a9efc39
DanielMysza <98398615+DanielMysza@users.noreply.github.com>
2022-05-12 13:37:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-class HerokuApp:
-    app_url = ""  # Fill your heroku app url here
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
